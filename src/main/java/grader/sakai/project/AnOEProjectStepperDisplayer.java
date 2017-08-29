d802e5018d417132fe666233cd817da06545bae4
pdewan <dewan@cs.unc.edu>
2017-08-29 16:43:21 -0400

    Changed edit to CompleteOEFrame

--- src/main/java/grader/sakai/project/AnOEProjectStepperDisplayer.java
+++ src/main/java/grader/sakai/project/AnOEProjectStepperDisplayer.java
@@ -3,20 +3,21 @@ package grader.sakai.project;
 import grader.assignment.GradingFeatureList;
 import grader.basics.settings.BasicGradingEnvironment;
 import util.trace.TraceableWarning;
+import bus.uigen.CompleteOEFrame;
 import bus.uigen.ObjectEditor;
 import bus.uigen.uiFrame;
 import bus.uigen.trace.IllegalSourceOfPropertyNotification;
 import bus.uigen.trace.UnknownPropertyNotification;
 
-public class AnOEProjectStepperDisplayer implements ProjectStepperDisplayer<uiFrame> {
-	public uiFrame display(ProjectStepper aProjectStepper) {
+public class AnOEProjectStepperDisplayer implements ProjectStepperDisplayer<CompleteOEFrame> {
+	public CompleteOEFrame display(ProjectStepper aProjectStepper) {
 //		ObjectEditor.setPropertyAttribute(AProjectStepper.class, "summary",  AttributeNames.SCROLLED, true);
 		
 		// because of delegating property change listeners
 		TraceableWarning.doNotWarn(UnknownPropertyNotification.class);
 		TraceableWarning.doNotWarn(IllegalSourceOfPropertyNotification.class);
 
-		uiFrame oeFrame = ObjectEditor.edit(aProjectStepper);
+		CompleteOEFrame oeFrame = ObjectEditor.edit(aProjectStepper);
 //		uiFrame oeFrame = ObjectEditor.tabEdit(aProjectStepper);
 
 		GradingFeatureList gradingFeatures = aProjectStepper.getGradingFeatures();
