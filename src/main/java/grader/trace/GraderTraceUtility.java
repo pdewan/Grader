59533a383f487cef08ef1a7c86da8d647a892faf
avitkus <avitkus7@gmail.com>
2018-11-02 18:06:50 -0400

    Cleaned up
    removed TurnOn property and extends GraderBasicsTestUtility

--- src/main/java/grader/trace/GraderTraceUtility.java
+++ src/main/java/grader/trace/GraderTraceUtility.java
@@ -2,23 +2,28 @@ package grader.trace;
 
 import framework.execution.ProcessRunner;
 import grader.execution.AnExecutionSpecification;
+import trace.grader.basics.GraderBasicsTraceUtility;
 import util.trace.ImplicitKeywordKind;
 import util.trace.Tracer;
 
-public class GraderTraceUtility {
+public class GraderTraceUtility extends GraderBasicsTraceUtility {
 	static boolean turnOn = true;
+	@Deprecated
 	public static boolean isTurnOn() {
 		return turnOn;
 	}
+	
+	@Deprecated
 	public static void setTurnOn(boolean turnOn) {
 		GraderTraceUtility.turnOn = turnOn;
 	}
 	public static void setTracing() {
+		GraderBasicsTraceUtility.setTracing();
 		Tracer.setImplicitPrintKeywordKind(ImplicitKeywordKind.OBJECT_PACKAGE_NAME);	
-		if (isTurnOn()) {
+//		if (isTurnOn()) {
 			Tracer.setKeywordPrintStatus(ProcessRunner.class, true);
 			Tracer.setKeywordPrintStatus(AnExecutionSpecification.class, true);
-		}
+//		}
 
 	}
 }
